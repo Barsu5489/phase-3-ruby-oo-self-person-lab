ce9d471e64e2471cb88760a785026e7a44dc1da4
barsu5489 <ebarsulai@gmail.com>
2023-02-12 10:03:24 +0300

    intialize and edit

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
-# your code goes here
+class Person
+    attr_reader :name, :happiness
+    attr_accessor :bank_account, :hygiene
+def initialize (name)
+    @name = name
+   @bank_account = 25
+   @happiness = 8
+   @hygiene = 8
+end
+def happiness=(value)
+    if (value > 10)
+        @happiness = 10
+    elsif (value < 1)
+        @happiness = 0
+    else 
+        @happiness = value
+        
+    end
+end
+
+end
