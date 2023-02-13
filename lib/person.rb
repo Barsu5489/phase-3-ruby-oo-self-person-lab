060b77f6eb1273563dc38a493dc27bc9450b8150
barsu5489 <ebarsulai@gmail.com>
2023-02-13 03:12:16 +0300

    OO Lab

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,6 @@
 class Person
     attr_reader :name, :happiness, :hygiene
-    attr_accessor :bank_account
+    attr_accessor :bank_account, :hygiene
 def initialize (name)
     @name = name
    @bank_account = 25
@@ -27,21 +27,44 @@ def hygiene=(value)
     end
 end
 def clean?
-   if @hygiene > 7
-    true
-    else
-    false
-end
+   self.hygiene > 7
+
 end
 def happy?
-    if @happiness > 7
-        true
-    else 
-        false
-    end
+    self.happiness > 7
+     
 end
 def get_paid(amount)
-@bank_account += amount
+self.bank_account += amount
 'all about the benjamins'
+end
+def take_bath
+    
+    self.hygiene += 4
+"♪ Rub-a-dub just relaxing in the tub ♫"
+end
+def work_out
+
+        self.happiness += 2
+    
+        self.hygiene -= 3
+"♪ another one bites the dust ♫"
+end
+def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+end
+def start_conversation(friend, topic)
+if topic == 'politics'
+    [self, friend].each {|p| p.happiness -= 2}
+    "blah blah partisan blah lobbyist"
+elsif topic == 'weather'
+    [self, friend].each {|p| p.happiness += 1}
+    
+    "blah blah sun blah rain"
+elsif topic != 'politics' || topic != 'weather'
+    "blah blah blah blah blah"
+end
 end
 end
